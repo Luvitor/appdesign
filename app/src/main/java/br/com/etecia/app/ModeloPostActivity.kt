3f8ee527d4aba899802126441f265de337eb9ae3
Luvitor <luan.silva524@etec.sp.gov.br>
2024-04-23 22:05:05 -0300

    novo

--- app/src/main/java/br/com/etecia/app/ModeloPostActivity.kt
+++ app/src/main/java/br/com/etecia/app/ModeloPostActivity.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ModeloFotoActivity : AppCompatActivity() {
+class ModeloPostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.modelo_foto)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.modelo_post)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.idCardProduto)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
