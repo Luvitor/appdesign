f6f075067ceb207734bdb3c8a644c73792e3e877
Luvitor <luan.silva524@etec.sp.gov.br>
2024-04-23 21:46:51 -0300

    novo

---
+++ app/src/main/java/br/com/etecia/app/ModeloFotoActivity.kt
@@ -0,0 +1,20 @@
+package br.com.etecia.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ModeloFotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.modelo_foto)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
